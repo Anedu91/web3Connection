bce50be31bf65f13db085d406ea913c7e60522f5
Angel Eduardo <40308240+Anedu91@users.noreply.github.com>
2022-06-26 12:55:29 -0300

    adding disconnect method

--- web3Connection.js
+++ web3Connection.js
@@ -47,12 +47,71 @@ async function onConnect() {
     console.log("Could not get a wallet connection", e);
     return;
   }
+
+  await fetchAccountData();
+}
+async function onDisconnect() {
+  console.log("Killing the wallet connection", provider);
+
+  // TODO: Which providers have close method?
+  if (provider.close) {
+    await provider.disconnect();
+
+    // If the cached provider is not cleared,
+    // WalletConnect will default to the existing session
+    // and does not allow to re-scan the QR code with a new wallet.
+    // Depending on your use case you may want or want not his behavir.
+    await web3Modal.clearCachedProvider();
+    provider = null;
+  }
+
+  selectedAccount = null;
+}
+
+// After a provider was selected
+async function fetchAccountData() {
+  // Get a Web3 instance for the wallet
+  const web3 = new Web3(provider);
+
+  console.log("Web3 instance is", web3);
+
+  // Get connected chain id from Ethereum node
+  const chainId = await web3.eth.getChainId();
+  // Load chain information over an HTTP API
+  const chainData = evmChains.getChain(chainId);
+  console.log("chaindata is", chainData);
+
+  // Get list of accounts of the connected wallet
+  const accounts = await web3.eth.getAccounts();
+
+  // MetaMask does not give you all accounts, only the selected account
+  console.log("Got accounts", accounts);
+  selectedAccount = accounts[0];
+
+  // Go through all accounts and get their ETH balance
+  const rowResolvers = accounts.map(async (address) => {
+    const balance = await web3.eth.getBalance(address);
+    // ethBalance is a BigNumber instance
+    // https://github.com/indutny/bn.js/
+    const ethBalance = web3.utils.fromWei(balance, "ether");
+    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
+    console.log("human balance", humanFriendlyBalance);
+    console.log("address", address);
+  });
+
+  // Because rendering account does its own RPC commucation
+  // with Ethereum node, we do not want to display any results
+  // until data for all accounts is loaded
+  await Promise.all(rowResolvers);
 }
 
 // Entry point
 window.addEventListener("load", async () => {
   init();
   document.querySelector("#btn-connect").addEventListener("click", onConnect);
+  document
+    .querySelector("#btn-disconnect")
+    .addEventListener("click", onDisconnect);
   // document
   //     .querySelector('#btn-disconnect')
   //     .addEventListener('click', onDisconnect)
