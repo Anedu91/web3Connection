0ac720cb4e04c9db01014cdbf08a7038d131cc58
Angel Eduardo <40308240+Anedu91@users.noreply.github.com>
2022-06-27 16:34:34 -0300

    adding contractr button

--- web3Connection.js
+++ web3Connection.js
@@ -122,6 +122,9 @@ window.addEventListener("load", async () => {
   document
     .querySelector("#btn-disconnect")
     .addEventListener("click", onDisconnect);
+  document
+    .querySelector("#btn-contract")
+    .addEventListener("click", fetchContractData);
   // document
   //     .querySelector('#btn-disconnect')
   //     .addEventListener('click', onDisconnect)
